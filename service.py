5793a553de1309d42413ec0a7e1e28c8cbbae228
Andrew Kuzyakov <andkuzyakov@gmail.com>
2025-10-19 23:09:33 +0300

    service_2

--- service.py
+++ service.py
@@ -24,3 +24,42 @@ def unholdbook(conn, pr, title, author):
     db_cursor.execute("""UPDATE books SET free=free+1 WHERE id=?""", (book_id,))
     conn.commit()
     print("бронь снята")
+
+def loanbook(conn, pr, title, author, date):
+    db_cursor=conn.cursor()
+    db_cursor.execute("""SELECT id, free FROM books WHERE title=? AND author=?""", (title, author))
+    book=db_cursor.fetchone()
+    if not book or book[1]<=0:
+        print("нет свободных экземпляров")
+    book_id=book[0]
+    db_cursor.execute("""SELECT COUNT(*) FROM loans WHERE pr=?""", (pr,))
+    if db_cursor.fetchone()[0]>=5:
+        print("у читателя уже 5 книг")
+    db_cursor.execute("""DELETE FROM holds WHERE pr=? AND book_id=?""", (pr, book_id))
+    db_cursor.execute("""INSERT INTO loans (pr, book_id, date) VALUES (?,?,?)""", (pr, book_id, date))
+    db_cursor.execute("""UPDATE books SET free=free-1 WHERE id=?""", (book_id,))
+    conn.commit()
+    print("книга выдана")
+
+def returnbook(conn, pr, title, author):
+    db_cursor = conn.cursor()
+    db_cursor.execute("""SELECT id FROM books WHERE title=? AND author=?""", (title, author))
+    book = db_cursor.fetchone()
+    if not book:
+        print("книга не найдена")
+    
+    book_id=book[0]
+    db_cursor.execute("""DELETE FROM loans WHERE pr=? AND book_id=?""", (pr, book_id))
+    db_cursor.execute("""UPDATE books SET free=free+1 WHERE id=?""", (book_id,))
+    conn.commit()
+    print("книга возвращена")
+
+def prosroch(conn):
+    db_cursor=conn.cursor()
+    db_cursor.execute("""
+        SELECT readers.pr, readers.full_name, books.title, books.author, loans.date AS date_return
+        FROM loans
+        JOIN books ON loans.book_id = books.id
+        JOIN readers ON loans.pr = readers.pr
+        WHERE date("now") > date(loans.date, "+14 day") """)
+    return db_cursor.fetchall()
